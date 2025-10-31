2b70b0b64dc472756d2669c8338ea713dd5b59a9
shank187 <aymane@aymane.localdomain>
2025-10-31 08:48:06 +0100

    ex03.0

---
+++ cpp_01/ex03/HumanB.cpp
@@ -0,0 +1,19 @@
+#include "HumanB.hpp"
+#include <iostream>
+#include <string>
+
+HumanB::HumanB(std::string name) : name(name), weapon(NULL) {}
+
+HumanB::~HumanB() {}
+
+void HumanB::setWeapon(Weapon &weapon) {
+    this->weapon = &weapon;
+}
+
+void HumanB::attack() const {
+    if (weapon) {
+        std::cout << name << " attacks with their " << weapon->getType() << std::endl;
+    } else {
+        std::cout << name << " is unarmed!" << std::endl;
+    }
+}
