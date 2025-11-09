af65c76681dd5d936cbe004d7f3793c36b81cdbb
shank187 <aymane@aymane.localdomain>
2025-11-09 08:09:04 +0100

    rmv deubugging in ex02 & ex03

--- cpp_02/ex03/Fixed.cpp
+++ cpp_02/ex03/Fixed.cpp
@@ -2,39 +2,33 @@
 #include <iostream>
 
 Fixed::Fixed() : value(0){
-    // std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed& other)
 {
     this->value = other.value;
-    // std::cout << "Copy constructor called" <<  std::endl;
 }
 
 Fixed::Fixed(const int value)
 {
-    // std::cout << "Int constructor called" << std::endl;
     this->value = value << fractionalBits;
 }
 
 Fixed::Fixed(const float value)
 {
-    // std::cout << "Float constructor called" << std::endl;
     this->value = static_cast<int>(roundf(value * (1 << fractionalBits)));
 }
 
 Fixed::~Fixed()
 {
-    // std::cout << "Destructor called" << std::endl;
 }
+
 int Fixed::getRawBits() const
 {
-    // std::cout << "getRawBits member function called" << std::endl;
     return this->value;
 }
 void Fixed::setRawBits(int const raw)
 {
-    // std::cout << "setRawBits member function called" << std::endl;
     this->value = raw;
 }
 
@@ -55,7 +49,6 @@ std::ostream& operator<<(std::ostream& os, const Fixed& fixed)
 
 Fixed& Fixed::operator=(const Fixed& other)
 {
-    // std::cout << "Copy assignment operator called" << std::endl;
     if (this != &other)
         this->value = other.value;
     return *this;
